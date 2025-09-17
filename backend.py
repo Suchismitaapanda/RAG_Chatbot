fb8c145e4006d470ec55fbca28d5b9f24f2a0eb0
Suchismita Panda <suchi10panda@gmail.com>
2025-09-17 22:16:21 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -213,3 +213,4 @@ def retrieve_all_threads():
     for checkpoint in checkpointer.list(None):
         all_threads.add(checkpoint.config["configurable"]["thread_id"])
     return list(all_threads)
+
